5f6bf10d33902ac184f6e83e7371e61ad62d68a9
Casper Anton Poulsen <caap@itu.dk>
2021-03-18 14:30:57 +0100

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def tokenizer(df, include_summary=False):
     '''
     Input: 
@@ -5,10 +7,10 @@ def tokenizer(df, include_summary=False):
     Output:
         - A list of lists of tokens.
     '''
+    df = df.replace(np.nan, '', regex=True)
     if include_summary:
         df['reviewText'] = df.reviewText +  " " + df.summary
     
-    df = df[df.reviewText.notna()]
     reviews = [[word for word in sentence.split(" ")] for sentence in df.reviewText if len(sentence) > 1]
     
     return reviews
