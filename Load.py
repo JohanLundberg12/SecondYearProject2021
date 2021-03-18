ba335c07619d8eeef0f9085822976f65a90cb0ab
Casper Anton Poulsen <caap@itu.dk>
2021-03-18 12:04:54 +0100

    New function
    New function

---
+++ Load.py
@@ -0,0 +1,10 @@
+import gzip
+import json
+import pandas as pd
+
+def compressed_json_to_df(path):
+    return pd.concat([pd.json_normalize(json.loads(line)) for line in gzip.open(path)])
+
+def json_to_df(path):
+    return pd.concat([pd.json_normalize(json.loads(line)) for line in open(path)])
+	
--- open_json.py
+++
@@ -1,6 +0,0 @@
-import gzip
-import json
-import pandas as pd
-
-def compressed_json_to_df(path):
-    return pd.concat([pd.json_normalize(json.loads(line)) for line in gzip.open(path)])
